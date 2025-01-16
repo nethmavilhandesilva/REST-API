a17aa69d1309680bacd0bc99b50804ee62761146
Supun Sandeeptha <supunsandeeptha@Supuns-MacBook-Air.local>
2025-01-16 23:08:35 +1100

    adding comments to the server.js file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
+// variable for the listening port of the server
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+// server listen port
 server.listen(port);
